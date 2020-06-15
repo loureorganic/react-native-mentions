85ac3709f425acb2ef938f99f6d0f5bfc0fece53
Jean Jesus <jean_jesus@ecglobal.com>
2020-06-15 17:35:23 -0300

    fix import

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _MentionsTextInput = _interopRequireDefault(require("./lib/MentionsTextInput.min"));
+var _MentionsTextInput = _interopRequireDefault(require("./lib/MentionsTextInput"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
