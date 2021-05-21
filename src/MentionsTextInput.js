6e0cd59b88b1e10a4c531b53d95528d359885c43
Bruno Vogel <bruno_vogel@ecgs.com>
2021-05-21 12:46:55 -0300

    Set useNativeDriver to false to prevent Error

--- src/MentionsTextInput.js
+++ src/MentionsTextInput.js
@@ -112,7 +112,7 @@ const MentionsTextInput = (props, forwardedRef) => {
     Animated.timing(suggestionRowHeight, {
       toValue: 0,
       duration: 100,
-      useNativeDriver: true,
+      useNativeDriver: false,
     }).start();
   }, []);
 
@@ -124,7 +124,7 @@ const MentionsTextInput = (props, forwardedRef) => {
     Animated.timing(suggestionRowHeight, {
       toValue: height != null ? height : props.suggestionRowHeight,
       duration: 100,
-      useNativeDriver: true
+      useNativeDriver: false
     }).start();
   }, [props.suggestionRowHeight]);
 
